41e7d98747d7c5cb7dffb59d17962b8ba8818937
Dario Arrieta <arrieta.dario@hotmail.com>
2024-01-23 16:26:53 -0500

    main---> add strategy example file

--- src/models/Strategy.ts
+++ src/models/Strategy.ts
@@ -2,15 +2,6 @@ import { Candle } from "./Candle";
 import { Interval } from "./Interval";
 import { PositionSide } from "./Position";
 
-export interface StrategyResponse {
-	shouldTrade: PositionSide | null;
-	sl: number;
-	tp: number;
-	tr?: number;
-	callback?: number;
-	stg?: string;
-}
-
 export interface Strategy {
 	name: string;
 	lookBackLength: number;
@@ -20,6 +11,25 @@ export interface Strategy {
 		pair?: string;
 	}) => StrategyResponse;
 }
+export interface StrategyResponse {
+	shouldTrade: PositionSide | null;
+	name: string;
+	sl: {
+		val?: number;
+		quitCriteria?: StrategyQuitCriteria;
+	};
+	tp: {
+		val?: number;
+		quitCriteria?: StrategyQuitCriteria;
+	};
+	tr?: {
+		val: number;
+		callback: number;
+	};
+}
+export type StrategyQuitCriteria = (props: {
+	candlestick: Candle[];
+}) => boolean;
 
 export interface StrategyStat {
 	name: string;
---
+++ src/strategies/strategy.example.ts
@@ -0,0 +1,47 @@
+import { rsi } from "technicalindicators";
+import { Interval } from "../models/Interval";
+import {
+	Strategy,
+	StrategyResponse,
+	StrategyQuitCriteria,
+} from "../models/Strategy";
+
+const strategyQuitCriteria: StrategyQuitCriteria = ({ candlestick }) => {
+	let response: boolean = false;
+	if (candlestick.length) {
+		response = true;
+	}
+	return response;
+};
+const STG_NAME = "stg_name";
+
+const stg: Strategy = {
+	name: STG_NAME,
+	lookBackLength: Interval["1d"] / Interval["5m"],
+	interval: Interval["5m"],
+	validate: ({ candlestick, pair }) => {
+		const response: StrategyResponse = {
+			shouldTrade: null,
+			name: STG_NAME,
+			sl: { val: 1 / 100 }, // Can be a static value or quit criteria function
+			tp: { quitCriteria: strategyQuitCriteria }, // Can be a static value or quit criteria function
+			tr: { val: 1 / 100, callback: 1 / 100 }, // add optionals tr and callback rate for adding trailing order to your position
+		};
+
+		if (candlestick.length < Interval["1d"] / Interval["5m"]) return response;
+
+		//Add your trading strategy here modifying shouldTrade, sl, tp, tr, callback
+		let condition = false;
+
+		if (condition) {
+			response.shouldTrade = "LONG";
+		}
+		if (condition) {
+			response.shouldTrade = "SHORT";
+		}
+
+		return response;
+	},
+};
+
+export default stg;
