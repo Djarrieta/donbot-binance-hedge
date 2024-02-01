a3c20ed85b73c55db5cbea0546744404d0f7ccb3
Dario Arrieta <arrieta.dario@hotmail.com>
2024-02-01 18:02:11 -0500

    main---> improve rsiOverTrade

--- src/strategies/rsiOverTrade.ts
+++ src/strategies/rsiOverTrade.ts
@@ -19,7 +19,7 @@ const stg: Strategy = {
 		if (candlestick.length < Context.lookBackLength) return response;
 
 		const MIN_VOL = 5 / 100;
-		const MIN_RSI = 60;
+		const MIN_RSI = 40;
 
 		const volatility = getVolatility({ candlestick });
 		const closePrices = candlestick.map((candle) => candle.close);
@@ -29,15 +29,15 @@ const stg: Strategy = {
 
 		if (
 			volatility >= MIN_VOL &&
-			rsiArray[rsiArray.length - 1] >= 100 - MIN_RSI &&
-			rsiArray[rsiArray.length - 2] < 100 - MIN_RSI
+			rsiArray[rsiArray.length - 1] >= MIN_RSI &&
+			rsiArray[rsiArray.length - 2] < MIN_RSI
 		) {
 			response.shouldTrade = "LONG";
 		}
 		if (
 			volatility >= MIN_VOL &&
-			rsiArray[rsiArray.length - 1] <= MIN_RSI &&
-			rsiArray[rsiArray.length - 2] > MIN_RSI
+			rsiArray[rsiArray.length - 1] <= 100 - MIN_RSI &&
+			rsiArray[rsiArray.length - 2] > 100 - MIN_RSI
 		) {
 			response.shouldTrade = "SHORT";
 		}
@@ -55,13 +55,13 @@ export default stg;
 // │             sl │ 0.50%               │
 // │             tp │ 0.50%               │
 // │       lookBack │ 1440                │
-// │      startTime │ 2024 01 31 17:11:15 │
+// │      startTime │ 2024 01 31 17:54:14 │
 // │       interval │ 1m                  │
 // │ maxTradeLength │ 1000                │
 // │            fee │ 0.05%               │
-// │      avWinRate │ 53.53%              │
-// │          avPnl │ -0.01%              │
-// │       totalPnl │ -17.35%             │
-// │      tradesQty │ 1203                │
+// │      avWinRate │ 52.86%              │
+// │          avPnl │ -0.02%              │
+// │       totalPnl │ -24.76%             │
+// │      tradesQty │ 1120                │
 // │  avTradeLength │ 7                   │
 // └────────────────┴─────────────────────┘
