ffa6ec408734abab4b9c4ad60fcff7f788a935d7
Dario Arrieta <arrieta.dario@hotmail.com>
2024-03-01 14:50:55 -0500

    test--->  sl 10%

--- src/models/Context.ts
+++ src/models/Context.ts
@@ -29,7 +29,7 @@ export class Context {
 	public static minVolatility = 10 / 100;
 	public static minAmountToTrade = 6;
 	public static fee = 0.0005;
-	public static defaultSL = 5 / 100;
+	public static defaultSL = 10 / 100;
 	public static defaultTP = 1 / 100;
 	public static defaultTR = 0.6 / 100;
 	public static defaultCB = 0.5 / 100;
