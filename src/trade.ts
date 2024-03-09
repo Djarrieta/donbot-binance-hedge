184c93f9890d99cea128d2e71e10b4bbbbf4f7a5
Dario Arrieta <arrieta.dario@hotmail.com>
2024-03-09 12:04:41 -0500

    main---> add try catch to cron tasks

--- src/trade.ts
+++ src/trade.ts
@@ -24,94 +24,101 @@ export const trade = async () => {
 	);
 
 	cron.schedule(CronInterval["4h"], async () => {
-		Context.resetInstance();
-		const context = await Context.getInstance();
+		try {
+			Context.resetInstance();
+			const context = await Context.getInstance();
 
-		context.symbolList = await getSymbolList();
-		for (const symbol of context.symbolList) {
-			subscribeToSymbolUpdates({
-				pair: symbol.pair,
-				interval: Context.interval,
-			});
-		}
+			context.symbolList = await getSymbolList();
+			for (const symbol of context.symbolList) {
+				subscribeToSymbolUpdates({
+					pair: symbol.pair,
+					interval: Context.interval,
+				});
+			}
 
-		console.log(
-			getDate().dateString,
-			context.symbolList.length + " symbols updated!"
-		);
+			console.log(
+				getDate().dateString,
+				context.symbolList.length + " symbols updated!"
+			);
 
-		context.userList = await getUserList();
-		console.log(getDate().dateString, "User list updated!");
-		console.log(
-			"Users: " + context.userList.map((u) => u.name?.split(" ")[0]).join(", ")
-		);
-		updateStrategyStat();
+			context.userList = await getUserList();
+			console.log(getDate().dateString, "User list updated!");
+			console.log(
+				"Users: " +
+					context.userList.map((u) => u.name?.split(" ")[0]).join(", ")
+			);
+			updateStrategyStat();
 
-		for (const user of context.userList) {
-			await positionManageExisting({ user });
-		}
-		for (const user of context.userList) {
-			console.log(user.text);
-		}
+			for (const user of context.userList) {
+				await positionManageExisting({ user });
+			}
+			for (const user of context.userList) {
+				console.log(user.text);
+			}
+		} catch (_) {}
 	});
 
 	cron.schedule(CronInterval["5m"], async () => {
-		const context = await Context.getInstance();
+		try {
+			const context = await Context.getInstance();
 
-		await delay(1000);
-		console.log("");
-		console.log(getDate().dateString, "Checking for trades!");
+			await delay(1000);
+			console.log("");
+			console.log(getDate().dateString, "Checking for trades!");
 
-		await markUnreadySymbols();
-		await getSymbolListVolatility();
+			await markUnreadySymbols();
+			await getSymbolListVolatility();
 
-		const readySymbols = [...context.symbolList]
-			.filter((s) => s.isReady && !s.isLoading)
-			.sort((a, b) => Number(b.volatility) - Number(a.volatility));
+			const readySymbols = [...context.symbolList]
+				.filter((s) => s.isReady && !s.isLoading)
+				.sort((a, b) => Number(b.volatility) - Number(a.volatility));
 
-		const { text: tradeArrayText, tradeArray } = await checkForTrades({
-			readySymbols,
-			interval: Context.interval,
-			strategyStats: context.strategyStats,
-			chosenStrategies,
-		});
+			const { text: tradeArrayText, tradeArray } = await checkForTrades({
+				readySymbols,
+				interval: Context.interval,
+				strategyStats: context.strategyStats,
+				chosenStrategies,
+			});
 
-		if (tradeArray.length) {
-			console.log(tradeArrayText);
+			if (tradeArray.length) {
+				console.log(tradeArrayText);
 
-			for (const user of context.userList) {
-				for (const trade of tradeArray) {
-					trade.stgResponse.shouldTrade &&
-						positionManageNew({
-							user,
-							symbol: trade.symbol,
-							shouldTrade: trade.stgResponse.shouldTrade,
-							sl: trade.stgResponse.sl,
-							tp: Number(trade.stgResponse.tp),
-							tr: Number(trade.stgResponse.tr),
-							cb: Number(trade.stgResponse.cb),
-						});
+				for (const user of context.userList) {
+					for (const trade of tradeArray) {
+						trade.stgResponse.shouldTrade &&
+							positionManageNew({
+								user,
+								symbol: trade.symbol,
+								shouldTrade: trade.stgResponse.shouldTrade,
+								sl: trade.stgResponse.sl,
+								tp: Number(trade.stgResponse.tp),
+								tr: Number(trade.stgResponse.tr),
+								cb: Number(trade.stgResponse.cb),
+							});
+					}
 				}
+			} else {
+				console.log("No trades found");
 			}
-		} else {
-			console.log("No trades found");
-		}
 
-		await delay(5000);
-		context.userList = await getUserList();
-		for (const user of context.userList) {
-			positionManageExisting({ user });
-		}
-		context.userList = await getUserList();
-		for (const user of context.userList) {
-			console.log(user.text);
-		}
+			await delay(5000);
+			context.userList = await getUserList();
+			for (const user of context.userList) {
+				positionManageExisting({ user });
+			}
+			context.userList = await getUserList();
+			for (const user of context.userList) {
+				console.log(user.text);
+			}
+		} catch (_) {}
 	});
 
 	cron.schedule(CronInterval["15m"], async () => {
-		console.log("");
-		console.log(getDate().dateString, "Updating symbols");
-		await updateUnreadySymbols();
+		try {
+			console.log("");
+			console.log(getDate().dateString, "Updating symbols");
+			await updateUnreadySymbols();
+		} catch (_) {}
 	});
 };
 
