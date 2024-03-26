b8e22d9d51211ea59a881e995ef524c1b26494c9
Dario Arrieta <arrieta.dario@hotmail.com>
2024-03-25 20:10:34 -0500

    test---> remove ema trend

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -2,4 +2,4 @@ import emaTrend5m from "./emaTrend5m";
 import rsiDivergency5m from "./rsiDivergency5m";
 import mfiDivergency5m from "./mfiDivergency5m";
 
-export const chosenStrategies = [emaTrend5m, rsiDivergency5m, mfiDivergency5m];
+export const chosenStrategies = [rsiDivergency5m, mfiDivergency5m];
