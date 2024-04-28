c80b094b69268d31d9c2b0379eeea45f0830cb16
Dario Arrieta <arrieta.dario@hotmail.com>
2024-04-28 12:40:17 -0500

    dev---> add trade lenght, fix logs

--- src/backtestAcc.ts
+++ src/backtestAcc.ts
@@ -1,4 +1,5 @@
 import { Context } from "./models/Context";
+import { Interval } from "./models/Interval";
 import { Position } from "./models/Position";
 import { Strategy, StrategyStat } from "./models/Strategy";
 import { Symbol } from "./models/Symbol";
@@ -16,22 +17,9 @@ export const backtestAcc = async ({
 	strategies: Strategy[];
 	log?: boolean;
 }) => {
-	const startTime =
-		getDate().dateMs - Context.lookBackLengthBacktest * Context.interval;
-
-	log &&
-		console.table({
-			stgName: strategies.map((s) => s.stgName).join(", "),
-			startTime: getDate(startTime).dateString,
-			endTime: getDate().dateString,
-			lookBack: Context.lookBackLengthBacktest,
-			interval: Context.interval,
-			maxTradeLength: Context.maxTradeLength,
-			fee: formatPercent(Context.fee),
-		});
-
 	const completeSymbolList = await getCompletePairList(false);
 	console.log("Getting symbol candlesticks...");
+	Context.lookBackLengthBacktest = (30 * Interval["1d"]) / Interval["5m"];
 
 	for (
 		let symbolIndex = 0;
@@ -47,234 +35,275 @@ export const backtestAcc = async ({
 			apiLimit: Context.candlestickAPILimit,
 		});
 		completeSymbolList[symbolIndex].candlestick = candlestick;
-		log &&
-			console.log(
-				formatPercent(symbolIndex / completeSymbolList.length) +
-					" " +
-					symbol.pair
-			);
+		console.log(
+			formatPercent(symbolIndex / completeSymbolList.length) + " " + symbol.pair
+		);
 	}
-	let openPosition: Position | null = null;
-	const closedPositions: Position[] = [];
 
-	let candleIndex = Context.lookBackLength;
-	let maxAccPnl = 0;
-	let minAccPnl = 0;
-	let accPnl = 0;
+	const slArray = [10 / 100, 9 / 100, 8 / 100];
+	const tpArray = [1 / 100, 2 / 100, 3 / 100];
+	const maxTradeLengthArray = [50, 100, 150, 200, 250, 300, 350];
 
-	do {
-		const readySymbols: Symbol[] = completeSymbolList.map((s) => {
-			return {
-				...s,
-				isReady: true,
-				isLoading: false,
-				candlestick: [
-					...s.candlestick.slice(
-						candleIndex - Context.lookBackLength,
-						candleIndex
-					),
-				],
-				pricePrecision: 0,
-				quantityPrecision: 0,
-			};
-		});
+	const startTime =
+		getDate().dateMs - Context.lookBackLengthBacktest * Context.interval;
 
-		const strategyStats: StrategyStat[] = [
-			...chosenStrategies.map((s) => {
-				return {
-					stgName: s.stgName,
-					status: true,
-					trades: 10,
-					avPnl: 1,
-					winRate: "100%",
-				};
-			}),
-		];
+	console.table({
+		stgName: strategies.map((s) => s.stgName).join(", "),
+		startTime: getDate(startTime).dateString,
+		endTime: getDate().dateString,
+		lookBack: Context.lookBackLengthBacktest,
+		interval: Context.interval,
+		maxTradeLength: Context.maxTradeLength,
+		fee: formatPercent(Context.fee),
+	});
 
-		const { tradeArray } = await checkForTrades({
-			readySymbols,
-			interval: Context.interval,
-			strategyStats,
-			chosenStrategies,
-			logs: false,
-		});
+	const result = [];
+	for (const maxTradeLength of maxTradeLengthArray) {
+		for (const tp of slArray) {
+			for (const sl of tpArray) {
+				Context.defaultTP = tp;
+				Context.defaultSL = sl;
+				Context.maxTradeLength = maxTradeLength;
 
-		if (openPosition !== null) {
-			const symbolOpened = readySymbols.find(
-				(s) => s.pair === openPosition?.pair
-			);
+				let openPosition: Position | null = null;
+				const closedPositions: Position[] = [];
 
-			if (!symbolOpened) continue;
-			const lastCandle =
-				symbolOpened.candlestick[symbolOpened.candlestick.length - 1];
+				let candleIndex = Context.lookBackLength;
+				let maxAccPnl = 0;
+				let minAccPnl = 0;
+				let accPnl = 0;
 
-			const stopLoss =
-				openPosition.positionSide === "LONG"
-					? openPosition.entryPriceUSDT * (1 - Context.defaultSL)
-					: openPosition.entryPriceUSDT * (1 + Context.defaultSL);
-			const takeProfit =
-				openPosition.positionSide === "LONG"
-					? openPosition.entryPriceUSDT * (1 + Context.defaultTP)
-					: openPosition.entryPriceUSDT * (1 - Context.defaultTP);
+				do {
+					const readySymbols: Symbol[] = completeSymbolList.map((s) => {
+						return {
+							...s,
+							isReady: true,
+							isLoading: false,
+							candlestick: [
+								...s.candlestick.slice(
+									candleIndex - Context.lookBackLength,
+									candleIndex
+								),
+							],
+							pricePrecision: 0,
+							quantityPrecision: 0,
+						};
+					});
 
-			if (
-				(openPosition.positionSide === "LONG" &&
-					(lastCandle.low <= stopLoss || lastCandle.close <= stopLoss)) ||
-				(openPosition.positionSide === "SHORT" &&
-					(lastCandle.high >= stopLoss || lastCandle.close >= stopLoss))
-			) {
-				openPosition.pnl = -Context.defaultSL - Context.fee;
-				openPosition.len++;
-				accPnl += openPosition.pnl;
-				if (accPnl > maxAccPnl) maxAccPnl = accPnl;
-				if (accPnl < minAccPnl) minAccPnl = accPnl;
-				log &&
-					console.log(
-						formatPercent(
-							(candleIndex + 1) /
-								(Context.lookBackLengthBacktest + Context.lookBackLength)
-						) +
-							" " +
-							`${openPosition.pair} ${
-								getDate(openPosition.startTime).dateString
-							} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
-								openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
-							} E:${openPosition.entryPriceUSDT.toFixed(
-								3
-							)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
-								3
-							)}-PNL:${formatPercent(openPosition.pnl)}-ACC:${formatPercent(
-								accPnl
-							)}`
-					);
+					const strategyStats: StrategyStat[] = [
+						...chosenStrategies.map((s) => {
+							return {
+								stgName: s.stgName,
+								status: true,
+								trades: 10,
+								avPnl: 1,
+								winRate: "100%",
+							};
+						}),
+					];
 
-				closedPositions.push(openPosition);
-				openPosition = null;
-				candleIndex++;
+					const { tradeArray } = await checkForTrades({
+						readySymbols,
+						interval: Context.interval,
+						strategyStats,
+						chosenStrategies,
+						logs: false,
+					});
 
-				continue;
+					if (openPosition !== null) {
+						const symbolOpened = readySymbols.find(
+							(s) => s.pair === openPosition?.pair
+						);
+
+						if (!symbolOpened) continue;
+						const lastCandle =
+							symbolOpened.candlestick[symbolOpened.candlestick.length - 1];
+
+						const stopLoss =
+							openPosition.positionSide === "LONG"
+								? openPosition.entryPriceUSDT * (1 - Context.defaultSL)
+								: openPosition.entryPriceUSDT * (1 + Context.defaultSL);
+						const takeProfit =
+							openPosition.positionSide === "LONG"
+								? openPosition.entryPriceUSDT * (1 + Context.defaultTP)
+								: openPosition.entryPriceUSDT * (1 - Context.defaultTP);
+
+						if (
+							(openPosition.positionSide === "LONG" &&
+								(lastCandle.low <= stopLoss || lastCandle.close <= stopLoss)) ||
+							(openPosition.positionSide === "SHORT" &&
+								(lastCandle.high >= stopLoss || lastCandle.close >= stopLoss))
+						) {
+							openPosition.pnl = -Context.defaultSL - Context.fee;
+							openPosition.len++;
+							accPnl += openPosition.pnl;
+							if (accPnl > maxAccPnl) maxAccPnl = accPnl;
+							if (accPnl < minAccPnl) minAccPnl = accPnl;
+							log &&
+								console.log(
+									formatPercent(
+										(candleIndex + 1) /
+											(Context.lookBackLengthBacktest + Context.lookBackLength)
+									) +
+										" " +
+										`${openPosition.pair} ${
+											getDate(openPosition.startTime).dateString
+										} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
+											openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
+										} E:${openPosition.entryPriceUSDT.toFixed(
+											3
+										)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
+											3
+										)}-PNL:${formatPercent(
+											openPosition.pnl
+										)}-ACC:${formatPercent(accPnl)}`
+								);
+
+							closedPositions.push(openPosition);
+							openPosition = null;
+							candleIndex++;
+
+							continue;
+						}
+
+						if (
+							(openPosition.positionSide === "LONG" &&
+								(lastCandle.high >= takeProfit ||
+									lastCandle.close >= takeProfit)) ||
+							(openPosition.positionSide === "SHORT" &&
+								(lastCandle.low <= takeProfit ||
+									lastCandle.close <= takeProfit))
+						) {
+							openPosition.pnl = Context.defaultTP - Context.fee;
+							openPosition.len++;
+							accPnl += openPosition.pnl;
+							if (accPnl > maxAccPnl) maxAccPnl = accPnl;
+							if (accPnl < minAccPnl) minAccPnl = accPnl;
+							log &&
+								console.log(
+									formatPercent(
+										(candleIndex + 1) /
+											(Context.lookBackLengthBacktest + Context.lookBackLength)
+									) +
+										" " +
+										`${openPosition.pair} ${
+											getDate(openPosition.startTime).dateString
+										} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
+											openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
+										} E:${openPosition.entryPriceUSDT.toFixed(
+											3
+										)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
+											3
+										)}-PNL:${formatPercent(
+											openPosition.pnl
+										)}-ACC:${formatPercent(accPnl)}`
+								);
+							closedPositions.push(openPosition);
+							openPosition = null;
+							candleIndex++;
+
+							continue;
+						}
+						if (openPosition.len >= Context.maxTradeLength) {
+							const pnlWithoutFee =
+								openPosition.positionSide === "LONG"
+									? (lastCandle.close - openPosition.entryPriceUSDT) /
+									  openPosition.entryPriceUSDT
+									: (openPosition.entryPriceUSDT - lastCandle.close) /
+									  openPosition.entryPriceUSDT;
+							openPosition.len++;
+							openPosition.pnl = pnlWithoutFee - Context.fee;
+							accPnl += openPosition.pnl;
+							if (accPnl > maxAccPnl) maxAccPnl = accPnl;
+							if (accPnl < minAccPnl) minAccPnl = accPnl;
+							log &&
+								console.log(
+									formatPercent(
+										(candleIndex + 1) /
+											(Context.lookBackLengthBacktest + Context.lookBackLength)
+									) +
+										" " +
+										`${openPosition.pair} ${
+											getDate(openPosition.startTime).dateString
+										} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
+											openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
+										} E:${openPosition.entryPriceUSDT.toFixed(
+											3
+										)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
+											3
+										)}-PNL:${formatPercent(
+											openPosition.pnl
+										)}-ACC:${formatPercent(accPnl)}`
+								);
+							closedPositions.push(openPosition);
+							openPosition = null;
+							candleIndex++;
+
+							continue;
+						}
+						openPosition.len++;
+						candleIndex++;
+						continue;
+					}
+
+					if (tradeArray.length && !openPosition) {
+						const symbolOpened = readySymbols.find(
+							(s) => s.pair === tradeArray[0].symbol.pair
+						);
+						if (!symbolOpened) continue;
+						openPosition = {
+							pair: symbolOpened.pair,
+							coinQuantity: "0",
+							status: "PROTECTED",
+							startTime:
+								symbolOpened.candlestick[symbolOpened.candlestick.length - 1]
+									.openTime,
+							positionSide: tradeArray[0].stgResponse.shouldTrade || "LONG",
+							pnl: 0,
+							len: 0,
+							isHedgeUnbalance: false,
+							entryPriceUSDT: symbolOpened.currentPrice,
+						};
+						candleIndex++;
+						continue;
+					}
+
+					candleIndex++;
+				} while (candleIndex < Context.lookBackLengthBacktest);
+				const totalPositions = closedPositions.length;
+				const winningPositions = closedPositions.filter((p) => p.pnl > 0);
+				const winRate = winningPositions.length / totalPositions;
+
+				result.push({
+					maxTradeLength: Context.maxTradeLength,
+					sl: Context.defaultSL,
+					tp: Context.defaultTP,
+					totalPositions,
+					maxAccPnl,
+					minAccPnl,
+					accPnl,
+					winRate,
+				});
 			}
-
-			if (
-				(openPosition.positionSide === "LONG" &&
-					(lastCandle.high >= takeProfit || lastCandle.close >= takeProfit)) ||
-				(openPosition.positionSide === "SHORT" &&
-					(lastCandle.low <= takeProfit || lastCandle.close <= takeProfit))
-			) {
-				openPosition.pnl = Context.defaultTP - Context.fee;
-				openPosition.len++;
-				accPnl += openPosition.pnl;
-				if (accPnl > maxAccPnl) maxAccPnl = accPnl;
-				if (accPnl < minAccPnl) minAccPnl = accPnl;
-				log &&
-					console.log(
-						formatPercent(
-							(candleIndex + 1) /
-								(Context.lookBackLengthBacktest + Context.lookBackLength)
-						) +
-							" " +
-							`${openPosition.pair} ${
-								getDate(openPosition.startTime).dateString
-							} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
-								openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
-							} E:${openPosition.entryPriceUSDT.toFixed(
-								3
-							)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
-								3
-							)}-PNL:${formatPercent(openPosition.pnl)}-ACC:${formatPercent(
-								accPnl
-							)}`
-					);
-				closedPositions.push(openPosition);
-				openPosition = null;
-				candleIndex++;
-
-				continue;
-			}
-			if (openPosition.len >= Context.maxTradeLength) {
-				const pnlWithoutFee =
-					openPosition.positionSide === "LONG"
-						? (lastCandle.close - openPosition.entryPriceUSDT) /
-						  openPosition.entryPriceUSDT
-						: (openPosition.entryPriceUSDT - lastCandle.close) /
-						  openPosition.entryPriceUSDT;
-				openPosition.len++;
-				openPosition.pnl = pnlWithoutFee - Context.fee;
-				accPnl += openPosition.pnl;
-				if (accPnl > maxAccPnl) maxAccPnl = accPnl;
-				if (accPnl < minAccPnl) minAccPnl = accPnl;
-				log &&
-					console.log(
-						formatPercent(
-							(candleIndex + 1) /
-								(Context.lookBackLengthBacktest + Context.lookBackLength)
-						) +
-							" " +
-							`${openPosition.pair} ${
-								getDate(openPosition.startTime).dateString
-							} ${openPosition.pnl > 0 ? "WON " : "LOST"} ${
-								openPosition.positionSide === "LONG" ? "LONG " : "SHORT"
-							} E:${openPosition.entryPriceUSDT.toFixed(
-								3
-							)} SL:${stopLoss.toFixed(3)}-TP:${takeProfit.toFixed(
-								3
-							)}-PNL:${formatPercent(openPosition.pnl)}-ACC:${formatPercent(
-								accPnl
-							)}`
-					);
-				closedPositions.push(openPosition);
-				openPosition = null;
-				candleIndex++;
-
-				continue;
-			}
-			openPosition.len++;
-			candleIndex++;
-			continue;
 		}
+	}
+	result.sort((a, b) => b.accPnl - a.accPnl);
 
-		if (tradeArray.length && !openPosition) {
-			const symbolOpened = readySymbols.find(
-				(s) => s.pair === tradeArray[0].symbol.pair
-			);
-			if (!symbolOpened) continue;
-			openPosition = {
-				pair: symbolOpened.pair,
-				coinQuantity: "0",
-				status: "PROTECTED",
-				startTime:
-					symbolOpened.candlestick[symbolOpened.candlestick.length - 1]
-						.openTime,
-				positionSide: tradeArray[0].stgResponse.shouldTrade || "LONG",
-				pnl: 0,
-				len: 0,
-				isHedgeUnbalance: false,
-				entryPriceUSDT: symbolOpened.currentPrice,
-			};
-			candleIndex++;
-			continue;
-		}
-
-		candleIndex++;
-	} while (
-		candleIndex <
-		Context.lookBackLengthBacktest + Context.lookBackLength
+	console.table(
+		result.map((r) => ({
+			maxTradeLengthArray: formatPercent(r.maxTradeLength),
+			sl: formatPercent(r.sl),
+			tp: formatPercent(r.tp),
+			totalPositions: r.totalPositions,
+			maxAccPnl: formatPercent(r.maxAccPnl),
+			minAccPnl: formatPercent(r.minAccPnl),
+			accPnl: formatPercent(r.accPnl),
+			winRate: formatPercent(r.winRate),
+		}))
 	);
-	const totalPositions = closedPositions.length;
-	const winningPositions = closedPositions.filter((p) => p.pnl > 0);
-	const winRate = winningPositions.length / totalPositions;
-
-	log &&
-		console.table({
-			totalPositions,
-			maxAccPnl: formatPercent(maxAccPnl),
-			minAccPnl: formatPercent(minAccPnl),
-			accPnl: formatPercent(accPnl),
-			winRate: formatPercent(winRate),
-		});
 };
 
 await backtestAcc({
 	strategies: chosenStrategies,
-	log: true,
+	log: false,
 });
