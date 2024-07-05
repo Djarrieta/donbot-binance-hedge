6626ee7bc675f2ca0e9f1a8182aa7b1233fd620e
Dario Arrieta <arrieta.dario@hotmail.com>
2024-07-05 09:20:13 -0500

    main---> add sudo to script

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 git checkout main
 git pull
-docker rm -f donbot-c
-docker rmi donbot-i
-docker build -t donbot-i .
-docker run -d --name donbot-c --restart=unless-stopped donbot-i
+sudo docker rm -f donbot-c
+sudo docker rmi donbot-i
+sudo docker build -t donbot-i .
+sudo docker run -d --name donbot-c --restart=unless-stopped donbot-i
 clear
