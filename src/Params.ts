9cdc84f12856b7d6b75b47af842e9d227a5aacc8
Dario Arrieta <arrieta.dario@hotmail.com>
2024-07-12 08:47:55 -0500

    feat/dynamic-stgs--> set max  hedge pos

--- src/Params.ts
+++ src/Params.ts
@@ -9,7 +9,7 @@ export const params = {
 	defaultBE: 1 / 100, // it is not considered in backtest
 	breakevenAlert: 5 / 100, // it is not considered in backtest
 	maxProtectedPositions: 1,
-	maxHedgePositions: 30,
+	maxHedgePositions: 15,
 	lookBackLengthDynamicBacktest: (3 * Interval["1d"]) / Interval["5m"],
 
 	lookBackLengthBacktest: (90 * Interval["1d"]) / Interval["5m"],
