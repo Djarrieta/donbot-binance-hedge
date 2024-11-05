e877134b54e38c117dc3609af9010856aaee0219
Dario Arrieta <arrieta.dario@hotmail.com>
2024-11-05 18:48:48 -0500

    poc-oop--> add backtest

--- src/DDD/domain/TradingStrategyTester.ts
+++ src/DDD/domain/TradingStrategyTester.ts
@@ -98,27 +98,11 @@ export class TradingStrategyTester {
 		);
 		const pairList = this.backtestDataService.getPairList();
 
-		const totalLookBackLength =
-			(this.config.backtestEnd -
-				this.config.backtestStart -
-				this.config.interval * (1 + this.config.lookBackLength)) /
-			this.config.interval;
-		const progressTotal =
-			this.config.slArray.length *
-			this.config.tpArray.length *
-			this.config.maxTradeLengthArray.length *
-			totalLookBackLength;
-
-		const progressBar = new cliProgress.SingleBar(
-			{},
-			cliProgress.Presets.shades_classic
-		);
-		progressBar.start(progressTotal, 0);
-		let progress = 0;
 		for (const sl of this.config.slArray) {
 			for (const tp of this.config.tpArray) {
 				for (const maxTradeLength of this.config.maxTradeLengthArray) {
-					const closedPositions: PositionBT[] = [];
+					const positions: PositionBT[] = [];
+					const trades: Trade[] = [];
 
 					let start = this.config.backtestStart;
 					let end =
@@ -130,107 +114,210 @@ export class TradingStrategyTester {
 						this.config.lookBackLength * this.config.interval;
 
 					do {
-						const candlesticksAllSymbols =
-							this.backtestDataService.getCandlestick({
-								start,
-								end,
-							});
-						const trades: Trade[] = [];
-						for (const pair of pairList) {
-							const candlestick = candlesticksAllSymbols.filter(
-								(c) => c.pair === pair && c.openTime < endCandlestick
-							);
+						const snapTrades = this.processBacktestTrades({
+							start,
+							end,
+							pairList,
+							endCandlestick,
+						});
 
-							const profitStick = candlesticksAllSymbols.filter(
-								(c) => c.pair === pair && c.openTime >= endCandlestick
-							);
+						const snapPositions = this.processBacktestPositions({
+							trades,
+							sl,
+							tp,
+						});
 
-							for (const strategy of this.strategies) {
-								const stgResponse = strategy?.validate({
-									candlestick,
-									pair,
-								});
-								if (stgResponse.positionSide) {
-									trades.push({ ...stgResponse, profitStick });
-								}
-							}
-						}
-
-						tradeLoop: for (const trade of trades) {
-							const { profitStick, pair } = trade;
-
-							const entryPriceUSDT = profitStick[0].open;
-
-							let pnl = 0;
-							let stickIndex = 0;
-							let done = false;
-							do {
-								const candle = profitStick[stickIndex];
-								const stopLoss =
-									trade.positionSide === "LONG"
-										? entryPriceUSDT * (1 - sl)
-										: entryPriceUSDT * (1 + sl);
-								const takeProfit = tp
-									? trade.positionSide === "LONG"
-										? entryPriceUSDT * (1 + tp)
-										: entryPriceUSDT * (1 - tp)
-									: 0;
-
-								if (
-									(trade.positionSide === "LONG" &&
-										(candle.low <= stopLoss || candle.close <= stopLoss)) ||
-									(trade.positionSide === "SHORT" &&
-										(candle.high >= stopLoss || candle.close >= stopLoss))
-								) {
-									pnl = -this.config.riskPt - this.config.feePt;
-									done = true;
-								}
-
-								if (
-									(trade.positionSide === "LONG" &&
-										(candle.high >= takeProfit ||
-											candle.close >= takeProfit)) ||
-									(trade.positionSide === "SHORT" &&
-										(candle.low <= takeProfit || candle.close <= takeProfit))
-								) {
-									pnl = this.config.riskPt * (tp / sl) - this.config.feePt;
-									done = true;
-								}
-
-								stickIndex++;
-							} while (done !== true && stickIndex < profitStick.length - 1);
-
-							if (pnl === 0) {
-								const lastPrice = profitStick[profitStick.length - 1].close;
-								const pnlGraph =
-									(trade.positionSide === "LONG"
-										? lastPrice - profitStick[0].open
-										: profitStick[0].open - lastPrice) / profitStick[0].open;
-
-								pnl = this.config.riskPt * (pnlGraph / sl) - this.config.feePt;
-							}
-							closedPositions.push({
-								pair,
-								positionSide: trade.positionSide as PositionSide,
-								startTime: profitStick[0].openTime,
-								entryPriceUSDT: profitStick[0].open,
-								pnl,
-								tradeLength: stickIndex,
-								stgName: trade.stgName,
-							});
-						}
-
+						positions.push(...snapPositions);
+						trades.push(...snapTrades);
 						start += this.config.interval;
 						end += this.config.interval;
 						endCandlestick += this.config.interval;
-						progress++;
-						progressBar.update(progress);
 					} while (end < this.config.backtestEnd);
-					console.log(closedPositions);
+					const stats = this.processStats({
+						positions,
+						pairList,
+					});
+
+					console.log({ sl, tp, maxTradeLength, ...stats });
 				}
 			}
 		}
-		progressBar.stop();
+	}
+
+	private processBacktestTrades({
+		start,
+		end,
+		pairList,
+		endCandlestick,
+	}: {
+		start: number;
+		end: number;
+		pairList: string[];
+		endCandlestick: number;
+	}) {
+		const candlesticksAllSymbols = this.backtestDataService.getCandlestick({
+			start,
+			end,
+		});
+		const trades: Trade[] = [];
+		for (const pair of pairList) {
+			const candlestick = candlesticksAllSymbols.filter(
+				(c) => c.pair === pair && c.openTime < endCandlestick
+			);
+
+			const profitStick = candlesticksAllSymbols.filter(
+				(c) => c.pair === pair && c.openTime >= endCandlestick
+			);
+
+			for (const strategy of this.strategies) {
+				const stgResponse = strategy?.validate({
+					candlestick,
+					pair,
+				});
+				if (stgResponse.positionSide) {
+					trades.push({ ...stgResponse, profitStick });
+				}
+			}
+		}
+
+		return trades;
+	}
+
+	private processBacktestPositions({
+		trades,
+		sl,
+		tp,
+	}: {
+		trades: Trade[];
+		sl: number;
+		tp: number;
+	}) {
+		const closedPositions: PositionBT[] = [];
+
+		for (const trade of trades) {
+			const { profitStick, pair } = trade;
+
+			const entryPriceUSDT = profitStick[0].open;
+
+			let pnl = 0;
+			let stickIndex = 0;
+			let done = false;
+			do {
+				const candle = profitStick[stickIndex];
+				const stopLoss =
+					trade.positionSide === "LONG"
+						? entryPriceUSDT * (1 - sl)
+						: entryPriceUSDT * (1 + sl);
+				const takeProfit = tp
+					? trade.positionSide === "LONG"
+						? entryPriceUSDT * (1 + tp)
+						: entryPriceUSDT * (1 - tp)
+					: 0;
+
+				if (
+					(trade.positionSide === "LONG" &&
+						(candle.low <= stopLoss || candle.close <= stopLoss)) ||
+					(trade.positionSide === "SHORT" &&
+						(candle.high >= stopLoss || candle.close >= stopLoss))
+				) {
+					pnl = -this.config.riskPt - this.config.feePt;
+					done = true;
+				}
+
+				if (
+					(trade.positionSide === "LONG" &&
+						(candle.high >= takeProfit || candle.close >= takeProfit)) ||
+					(trade.positionSide === "SHORT" &&
+						(candle.low <= takeProfit || candle.close <= takeProfit))
+				) {
+					pnl = this.config.riskPt * (tp / sl) - this.config.feePt;
+					done = true;
+				}
+
+				stickIndex++;
+			} while (done !== true && stickIndex < profitStick.length - 1);
+
+			if (pnl === 0) {
+				const lastPrice = profitStick[profitStick.length - 1].close;
+				const pnlGraph =
+					(trade.positionSide === "LONG"
+						? lastPrice - profitStick[0].open
+						: profitStick[0].open - lastPrice) / profitStick[0].open;
+
+				pnl = this.config.riskPt * (pnlGraph / sl) - this.config.feePt;
+			}
+			closedPositions.push({
+				pair,
+				positionSide: trade.positionSide as PositionSide,
+				startTime: profitStick[0].openTime,
+				entryPriceUSDT: profitStick[0].open,
+				pnl,
+				tradeLength: stickIndex,
+				stgName: trade.stgName,
+			});
+		}
+
+		return closedPositions;
+	}
+
+	private processStats({
+		positions,
+		pairList,
+	}: {
+		positions: PositionBT[];
+		pairList: string[];
+	}) {
+		let winningPairs: string[] = [];
+
+		for (let symbolIndex = 0; symbolIndex < pairList.length; symbolIndex++) {
+			const pair = pairList[symbolIndex];
+			const closedPosForSymbol = positions.filter((pos) => pos.pair === pair);
+			const tradesQty = closedPosForSymbol.length;
+			const totalPnl = closedPosForSymbol.reduce((acc, a) => acc + a.pnl, 0);
+			const avPnl = totalPnl / tradesQty || 0;
+
+			if (avPnl > 0) {
+				winningPairs.push(pair);
+			}
+		}
+
+		const closedPositionsWP = positions.filter((p) => {
+			return winningPairs.includes(p.pair);
+		});
+
+		const tradesQtyWP = closedPositionsWP.length;
+		const winningPositionsWP = closedPositionsWP.filter((p) => p.pnl > 0);
+		const winRateWP = winningPositionsWP.length / tradesQtyWP;
+		const accPnlWP = closedPositionsWP.reduce((acc, p) => acc + p.pnl, 0);
+		const avPnlWP = accPnlWP / tradesQtyWP || 0;
+
+		const closedPositionsAcc = [];
+		let openPosTime = 0;
+		for (const position of closedPositionsWP) {
+			if (position.startTime > openPosTime) {
+				closedPositionsAcc.push(position);
+				openPosTime =
+					position.startTime + position.tradeLength * this.config.interval;
+			}
+		}
+
+		const tradesQtyAcc = closedPositionsAcc.length;
+		const winningPositionsAcc = closedPositionsAcc.filter((p) => p.pnl > 0);
+		const winRateAcc = winningPositionsAcc.length / tradesQtyAcc;
+		const accPnlAcc = closedPositionsAcc.reduce((acc, p) => acc + p.pnl, 0);
+		const avPnlAcc = accPnlAcc / tradesQtyAcc || 0;
+
+		return {
+			tradesQtyWP,
+			winRateWP,
+			avPnlWP,
+			tradesQtyAcc,
+			winRateAcc,
+			avPnlAcc,
+			winningPairs,
+			closedPositionsAcc,
+			closedPositionsWP,
+		};
 	}
 
 	private validateTimeRanges(
