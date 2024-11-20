4532ed4df75ba3ebc77b21ace50e5d51b7ae6587
Dario Arrieta <arrieta.dario@hotmail.com>
2024-11-20 16:46:49 -0500

    poc-oop--> wip

--- src/config.ts
+++ src/config.ts
@@ -4,24 +4,24 @@ import type { Strategy } from "./domain/Strategy";
 import type { BacktestConfig } from "./domain/TradingStrategyTester";
 import { rsiDivergency5m } from "./strategies/rsiDivergency5m";
 
-export const DATA_BASE_NAME = "DB 15m.db";
+export const DATA_BASE_NAME = "DB.db";
 
 export const backtestConfig: BacktestConfig = {
 	backtestStart: getDate("2024 08 10 00:00:00" as DateString).dateMs,
 	backtestEnd: getDate("2024 10 20 00:00:00" as DateString).dateMs,
 	forwardTestEnd: getDate("2024 11 20 00:00:00" as DateString).dateMs,
-	interval: Interval["15m"],
+	interval: Interval["5m"],
 	lookBackLength: 200,
 	minAmountToTradeUSDT: 6,
 	apiLimit: 500,
-	slArray: [1 / 100, 2 / 100, 3 / 100, 4 / 100, 5 / 100],
+	slArray: [1 / 100],
 	tpArray: [10 / 100],
-	maxTradeLengthArray: [50, 100],
+	maxTradeLengthArray: [100],
 	riskPt: 0.5 / 100,
 	feePt: 0.0005,
 	steps: {
-		overrideHistoricalRecords: true,
-		overrideAlerts: true,
+		overrideHistoricalRecords: false,
+		overrideAlerts: false,
 	},
 };
 
