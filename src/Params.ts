b9f118d27d994e80ff6717d8d53759461d070820
Dario Arrieta <arrieta.dario@hotmail.com>
2024-11-20 06:45:54 -0500

    main---> fix params

--- src/Params.ts
+++ src/Params.ts
@@ -4,15 +4,9 @@ export const params = {
 	branch: "main" as "main" | "dev",
 
 	maxTradeLength: 100,
-	defaultSL: 2 / 100,
+	defaultSL: 1 / 100,
 	defaultTP: 10 / 100,
-	breakEventAlerts: [
-		{ alert: 1 / 100, value: 0.5 / 100, len: 1 },
-		{ alert: 3 / 100, value: 1 / 100, len: 1 },
-		{ alert: 5 / 100, value: 3 / 100, len: 1 },
-		{ alert: 7 / 100, value: 5 / 100, len: 1 },
-		{ alert: 9 / 100, value: 7 / 100, len: 1 },
-	], // it is not considered in backtest
+	breakEventAlerts: [], // it is not considered in backtest
 	maxProtectedPositions: 1,
 	maxHedgePositions: 15,
 	leverage: 10,
