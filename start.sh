b4d39c58c39c80d46a20ad8f77eea86eb6dfd87b
Dario Arrieta <arrieta.dario@hotmail.com>
2024-12-15 21:37:21 -0500

    main---> add volume

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ git pull
 sudo docker rm -f donbot-c
 sudo docker rmi donbot-i
 sudo docker build -t donbot-i .
-sudo docker run -d --name donbot-c --restart=unless-stopped donbot-i
+sudo docker run -d --name donbot-c  -v ./trade.db:/app/trade.db  --restart=unless-stopped donbot-i
 clear
