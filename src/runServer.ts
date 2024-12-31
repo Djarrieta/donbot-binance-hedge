3fe1ab674a49cbe1ab5928f2b27d3a0fbd800992
Dario Arrieta <arrieta.dario@hotmail.com>
2024-12-31 11:01:35 -0500

    main---> refactor server

---
+++ src/runServer.ts
@@ -0,0 +1,26 @@
+import { App } from "./server/app";
+import { Pairs } from "./server/pairs";
+
+Bun.serve({
+	port: 3000,
+	async fetch(req) {
+		const url = new URL(req.url);
+
+		if (url.pathname === "/pairs/") {
+			try {
+				const pair = url.searchParams.get("pair") || "";
+				const { head, body } = Pairs({ pair });
+				const html = App({ head, body });
+
+				return new Response(html, {
+					headers: { "Content-Type": "text/html" },
+				});
+			} catch (error) {
+				console.error("Error pairs:", error);
+				return new Response("Internal Server Error", { status: 500 });
+			}
+		}
+
+		return new Response("404 Not Found", { status: 404 });
+	},
+});
---
+++ src/server/App.ts
@@ -0,0 +1,10 @@
+export const App = ({ head, body }: { head: string; body: string }) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    ${head}
+</head>
+${body}
+</html>`;
