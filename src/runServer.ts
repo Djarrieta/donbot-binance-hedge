a47b79be4f6daf6602b17dc7ec6da665d8aebfcb
Dario Arrieta <arrieta.dario@hotmail.com>
2024-12-31 11:24:07 -0500

    main---> refactor app

--- src/runServer.ts
+++ src/runServer.ts
@@ -7,17 +7,10 @@ Bun.serve({
 		const url = new URL(req.url);
 
 		if (url.pathname.startsWith("/pairs/") && url.pathname.split("/")[2]) {
-			try {
-				const pair = url.pathname.split("/")[2] || "";
-				const { head, body } = Pairs({ pair });
+			const pair = url.pathname.split("/")[2];
+			const { head, body } = Pairs({ pair });
 
-				return new Response(App({ head, body }), {
-					headers: { "Content-Type": "text/html" },
-				});
-			} catch (error) {
-				console.error("Error pairs:", error);
-				return new Response("Internal Server Error", { status: 500 });
-			}
+			return App({ head, body });
 		}
 
 		return new Response("404 Not Found", { status: 404 });
--- src/server/App.ts
+++ src/server/App.ts
@@ -1,10 +1,21 @@
-export const App = ({ head, body }: { head: string; body: string }) => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    ${head}
-</head>
-${body}
-</html>`;
+export const App = ({ head, body }: { head: string; body: string }) => {
+	try {
+		const html = `
+                <!DOCTYPE html>
+                            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    ${head}
+                </head>
+                ${body}
+                </html>`;
+
+		return new Response(html, {
+			headers: { "Content-Type": "text/html" },
+		});
+	} catch (error) {
+		console.error("Error pairs:", error);
+		return new Response("Internal Server Error", { status: 500 });
+	}
+};
